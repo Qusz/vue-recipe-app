428e9f91d267a0067a241c67d12b21d136f9f200
unknown <de.gorchakov@gmail.com>
2022-10-28 12:02:47 +0300

    adds navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
 
-  <h1>Test header</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, distinctio omnis! Assumenda magnam veniam eum consequatur et velit fuga, temporibus ut libero perspiciatis atque repudiandae rerum non esse hic minus?</p>
-  
+  <TheHeader></TheHeader>
+
 </template>
 
 
 <script>
 
+import TheHeader from '@/components/TheHeader.vue'
+
+export default {
+    data() {
+        return {};
+    },
+    components: { TheHeader }
+}
+
 </script>
-
-<style>
-
-
-
-</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <header class="header">
+    <div class="container">
+      <nav class="navbar">
+        <div class="navbar__logo">
+          <a href="#">Recipes</a>
+        </div>
+        <ul class="navbar__items">
+          <li class="navbar__item"><a href="#">Home</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped lang="scss">
+
+@import '@/assets/styles/sass/components/TheHeader.scss';
+
+</style>
