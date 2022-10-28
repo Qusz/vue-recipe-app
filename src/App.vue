5671614d0744d6c4c9e8bacd367e9f57717854bb
unknown <de.gorchakov@gmail.com>
2022-10-28 10:33:34 +0300

    adds custom fonts

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <h1>Test header</h1>
+  <p>Test paragraph</p>
+  
+</template>
+
+
+<script>
+
+</script>
+
+<style>
+
+
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './assets/styles/sass/styles.scss'
+
 createApp(App).mount('#app')
