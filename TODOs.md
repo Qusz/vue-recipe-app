de245d20a18f8a6409c858d62c80c2eb655f4395
unknown <de.gorchakov@gmail.com>
2022-11-03 12:56:35 +0300

    adds basic logic for add new recipe page

--- TODOs.md
+++ TODOs.md
@@ -9,8 +9,10 @@ Create json-server script
 Render recipes on main page
 Add create recipe
 Add delete recipe 
-Add animation to add/remove ingredient
+Add animation to add/remove ingredient (there's a native Vue wave, look up in Notion notes)
 Change add recipe default input styling (incling focus)
+Add utility class instad of display flex everywhere
+Remove default indent on 1st line of textarea
 
 
 Refactor, so that recipe big card and add new recipe use the same component 
--- src/views/AddRecipe.vue
+++ src/views/AddRecipe.vue
@@ -6,45 +6,80 @@
         Add a new recipe
       </h1>
 
-      <label for="recipe-desc" class="add-recipe__title">
-        Recipe Description
-      </label>
-      <textarea 
-        class="add-recipe__description"
-        name="recipe-desc"
-        id="recipe-desc"
-        cols="80" 
-        rows="6"
-      > 
-      </textarea>
-
-      <h2 class="add-recipe__title">
-        Ingredients
-      </h2>
-      <div class="add-recipe__ingredients">
-        <ul class="add-recipe__items">
-          <li class="add-recipe__item"></li>
-        </ul>
-        <form class="add-recipe__ingredient">
-          <input type="text" class="add-recipe__ingredient-name" placeholder="Ingredient">
-          <input type="text" class="add-recipe__ingredient-qty" placeholder="Quantity">
-          <button type="submit" class="">+</button>
-        </form>
+      <div class="add-recipe__wrapper">
+        <label for="recipe-desc" class="add-recipe__title">
+          Recipe Description
+        </label>
+        <textarea
+          v-model="description"
+          class="add-recipe__description"
+          name="recipe-desc"
+          id="recipe-desc"
+          cols="80"
+          rows="6"
+        >
+        </textarea>
       </div>
 
-      <label for="recipe-inst" class="add-recipe__title">
-        Instructions
-      </label>
-      <textarea 
-        class="add-recipe__instructions"
-        name="recipe-inst"
-        id="recipe-inst"
-        cols="80" 
-        rows="15"
-      > 
-      </textarea>
+      <div class="add-recipe__wrapper">
+        <h2 class="add-recipe__title">
+          Ingredients
+        </h2>
 
-      <button class="btn btn-accent">
+        <div class="add-recipe__ingredients">
+
+          <ul class="add-recipe__items">
+            <li
+              v-for="(item, i) in ingredients"
+              :key="i" 
+              class="add-recipe__item"
+            >
+            {{ item.ingredient }}, {{ item.qty }}
+            </li>
+          </ul>
+
+          <form class="add-recipe__ingredient">
+            <input
+              v-model="ingredientName" 
+              type="text" 
+              class="add-recipe__ingredient-name" 
+              placeholder="Ingredient"
+            >
+            <input
+              v-model="ingredientQty"
+              type="text"
+              class="add-recipe__ingredient-qty"
+              placeholder="Qty"
+            >
+            <button
+              @click.prevent="addIngredient()"
+              type="submit"
+              class=""
+            >
+              +
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div class="add-recipe__wrapper">
+        <label for="recipe-inst" class="add-recipe__title">
+          Instructions
+        </label>
+        <textarea
+          v-model="instructions"
+          class="add-recipe__instructions"
+          name="recipe-inst"
+          id="recipe-inst"
+          cols="80"
+          rows="15"
+        >
+        </textarea>
+      </div>
+
+      <button 
+        class="btn btn-accent add-recipe__button"
+        @click="test()">
         Add recipe
       </button>
     </section>
@@ -58,20 +93,36 @@ export default {
   name: 'AddRecipe',
   data() {
     return {
+      currentId: 0,
       description: "",
+      ingredientName: "",
+      ingredientQty: null,
       ingredients: [],
-      instructions: ""
+      instructions: "",
+      fullRecipe: {
+        "id": this.currentId,
+        "name": "",
+        "desc": "",
+        "ingredients": [],
+        "instructions": ""
+      }
     }
   },
   methods: {
     addIngredient() {
-
+      const ingredient = {"ingredient": this.ingredientName, "qty": this.ingredientQty};
+      this.ingredients.push(ingredient);
+      this.ingredientName = "";
+      this.ingredientQty = null;
     },
     removeIngredient() {
 
     },
     submitRecipe() {
 
+    },
+    test() {
+      console.log(this.description, this.instructions);
     }
   }
 }
